37a8f24a2b47e9004d18c7d2064c2fcbc659caa7
DuruTiryaki <durutiryaki@gmail.com>
2024-12-09 18:16:11 +0000

    - junit dependency is added to pom.xml. Test files removed under the test folder. Test files (cards) are added into resources such as four.txt. Refactor warnings.

--- src/main/java/CardGame.java
+++ src/main/java/CardGame.java
@@ -1,5 +1,5 @@
-import java.io.File; // jaImport File Class for file access
-import java.io.FileNotFoundException; // Import for handling errors accessing pack file
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class CardGame {
@@ -32,7 +32,7 @@ public class CardGame {
         CardPack outputCardPack = new CardPack();
 
         // For each card give, add it to the pack
-        for(Card targetCard : listOfCards){
+        for (Card targetCard : listOfCards) {
             outputCardPack.addCard(targetCard);
         }
 
@@ -40,9 +40,7 @@ public class CardGame {
         return outputCardPack;
     }
 
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner cmdReader = new Scanner(System.in); // Scanner object to handle input
         String packFileName; // Store pack file path
         boolean fileReadFlag = false; // Flag set to true when the pack file is successfully read
@@ -54,39 +52,33 @@ public class CardGame {
         List<Card> cardList = new ArrayList<>(); // Stores cards from the pack file
         List<CardDeck> cardDecks = new ArrayList<>(); // List of all card decks
         CardPack packOfCards = null;
-
         // Check number of players
-        while(noOfPlayers <= 0){
+        while (noOfPlayers <= 0) {
             System.out.println("Please enter the number of players:");
             try {
                 noOfPlayers = Integer.parseInt(cmdReader.nextLine());
             } catch (NumberFormatException e) {
                 System.out.println("[!] Invalid Input - Must choose a whole number [!]");
             }
-
-            if (noOfPlayers <= 1){
+            if (noOfPlayers <= 1) {
                 System.out.println("[!] Invalid Input - Must be above one [!]");
             }
         }
-
         // Get pack file
-        while (!fileReadFlag){
+        while (!fileReadFlag) {
             System.out.println("Please enter location of pack to load:");
             packFileName = cmdReader.nextLine();
             try {
                 // Create Scanner
                 File fileObj = new File(packFileName);
                 Scanner myReader = new Scanner(fileObj);
-
                 //First loop to get total number of cards (this is needed to determine the length of the card array)
                 while (myReader.hasNextLine()) {
                     myReader.nextLine();
                     noOfCards += 1;
                 }
-
                 myReader = new Scanner(fileObj); // Reset scanner to first line
                 noOfCards = 0; // Reset for use in next loop
-
                 //Second loop to create the cards - Creates new card for each line in pack.txt
                 while (myReader.hasNextLine()) {
                     cardList.add(new Card(Integer.parseInt(myReader.nextLine())));
@@ -94,48 +86,41 @@ public class CardGame {
                 }
                 myReader.close();
                 fileReadFlag = true;
-
             }
             // Handler if file not found
             catch (FileNotFoundException e) {
                 System.out.println("[!] An error occurred while trying to access " + packFileName + " [!]");
             }
-
             // Construct the total card pack
             packOfCards = cardListToPack(cardList);
-
             // Final checks to ensure the pack file is valid, before proceeding with the game
-            if(fileReadFlag){
+            if (fileReadFlag) {
+                System.out.println("File is not valid!");
                 fileReadFlag = isFileValid(packOfCards, noOfPlayers);
             }
         }
-
         // Shuffle cards
         packOfCards.shuffle();
         cardList.clear();
-        for (Card targetCard : packOfCards.getCards()){
-            cardList.add(targetCard);
-        }
-
+        cardList.addAll(packOfCards.getCards());
         // DECK SETUP
         // Each deck will have 4 cards, same as the players, because...
         // The total number of cards is 8n ( 8 * the number of players)
         // The total number of cards allocated TO PLAYERS will be 4n (4 * the number of players), because each player gets 4 cards each.
         // That leaves us with 4n cards left (8n - 4n = 4n) as the total number of cards FOR DECKS
         // Because there is an equal number of decks to players, that means each single deck will have 4 cards (4n / n = 4)
-
-        for(int deckNum = 0; deckNum < noOfPlayers; deckNum++){
+        for (int deckNum = 0; deckNum < noOfPlayers; deckNum++) {
             List<Card> tempDeck = new ArrayList<>();
-            for (int i = 0; i < 4; i++){
+            for (int i = 0; i < 4; i++) {
                 // Collect 4 cards from the total deck
-                tempDeck.add(cardList.remove(0));
+                tempDeck.add(cardList.removeFirst()); // removes index of zero.
             }
             // Create a new deck with those 4 cards
             cardDecks.add(new CardDeck(tempDeck, deckNum + 1));
             System.out.println("setup deck " + (cardDecks.get(cardDecks.size() - 1)).getDeckID() + " with contents " + (cardDecks.get(cardDecks.size() - 1)).getDeckString());
         }
 
-        for(CardDeck deck : cardDecks){
+        for (CardDeck deck : cardDecks) {
             System.out.println("initial contents of deck " + deck.getDeckID() + ": " + deck.getDeckString());
         }
 
@@ -150,42 +135,41 @@ public class CardGame {
         // ('->' is the direction of cards, N is the number of players)
 
         // Set up Players
-        for(int playerNum = 0; playerNum < noOfPlayers; playerNum++){
+        for (int playerNum = 0; playerNum < noOfPlayers; playerNum++) {
             playerList.add(new Player(playerNum + 1));
             playerList.get(playerNum).setGiveDeck(cardDecks.get(playerNum)); // Set the 'give deck' reference to the deck that it shares its index with
 
             // For the first player, set its 'take deck' to be the last deck in the list
-            if(playerNum == 0){
+            if (playerNum == 0) {
                 playerList.get(playerNum).setTakeDeck(cardDecks.get(cardDecks.size() - 1));
-            }
-            else{
+            } else {
                 // Otherwise, set the 'take deck' to be the one immediately behind the player.
                 playerList.get(playerNum).setTakeDeck(cardDecks.get(playerNum - 1));
             }
         }
 
         // Each player will be given 4 cards, so do this 4 times.
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             // Traverse player list and for each player give them a card
-            for(Player targetPlayer : playerList){
+            for (Player targetPlayer : playerList) {
                 targetPlayer.addCardToHand(cardList.remove(0));
             }
         }
 
         //Start all Player Threads, this begins the game
-        for (Player targetPlayer : playerList){
+        for (Player targetPlayer : playerList) {
             targetPlayer.start();
         }
 
         boolean allThreadsAlive = true;
         //Thread manager loop. This continuously checks for a thread that has completed. If so, it informs all other threads that player thread has won and stops their execution.
-        while(allThreadsAlive){
+        while (allThreadsAlive) {
             // For each player object, check if that thread has won
-            for (Player targetPlayer : playerList){
+            for (Player targetPlayer : playerList) {
                 // If so, go through each player object and inform them that there has been a winner
-                if(targetPlayer.getWinState()){
+                if (targetPlayer.getWinState()) {
                     playerList.remove(targetPlayer);
-                    for (Player remainingPlayers : playerList){
+                    for (Player remainingPlayers : playerList) {
                         // Declare Winner method stops the game loop in the thread from executing further, and informs them of the victor.
                         remainingPlayers.declareWinner(targetPlayer.getPlayerId());
                     }
@@ -195,16 +179,14 @@ public class CardGame {
                 }
             }
             // Sleep to prevent starvation of the child threads.
-            try{
+            try {
                 Thread.sleep(10);
-            }
-            catch (InterruptedException e){
-                e.printStackTrace();
+            } catch (InterruptedException e) {
+                System.out.println("Error is occurred while Tread mechanism. Detail:" + e.getMessage());
             }
         }
-
         //Finally, get each deck to output it's final state.
-        for (CardDeck deck : cardDecks){
+        for (CardDeck deck : cardDecks) {
             deck.writeOutput();
         }
     }
